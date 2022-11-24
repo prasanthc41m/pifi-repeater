2b0b7d2700ecec871411a18eac00b222e33ddec0
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-11-24 23:26:10 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo systemctl mask networking.service dhcpcd.service
+sudo systemctl mask networking.service dhmvcd.service
 sudo mv /etc/network/interfaces /etc/network/interfaces.bak
 sudo mv /etc/resolvconf.conf /etc/resolvconf.conf.bak
 sudo sed -i '1i resolvconf=NO'  /etc/resolvconf.conf
@@ -12,24 +12,24 @@ cd /tmp
 git clone https://github.com/prasanthc41m/wifite-repeater.git
 cd wifite-repeater
 
-sudo cp wpa_supplicant-wlan0.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+sudo mv wpa_supplicant-wlan0.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 sudo systemctl disable wpa_supplicant.service
 sudo systemctl enable wpa_supplicant@wlan0.service
 
-sudo cp wpa_supplicant-wlan1.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+sudo mv wpa_supplicant-wlan1.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 sudo systemctl disable wpa_supplicant.service
 sudo systemctl enable wpa_supplicant@wlan1.service
 
-sudo cp 08-wlan0.network /etc/systemd/network/08-wlan0.network
-sudo cp 12-wlan1.network /etc/systemd/network/12-wlan1.network
+sudo mv 08-wlan0.network /etc/systemd/network/08-wlan0.network
+sudo mv 12-wlan1.network /etc/systemd/network/12-wlan1.network
 
 sudo -H pip install wifi
 sudo apt install python-flask python3-flask -y
 
-cp -r webui/ ~/
-sudo cp webserver.service /lib/systemd/system/webserver.service
+mv -r webui/ ~/
+sudo mv webserver.service /lib/systemd/system/webserver.service
 systemctl daemon-reload
 sudo systemctl enable server
 sudo systemctl start server
