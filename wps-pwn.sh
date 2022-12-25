df28412e81f2989f11ed7b905d6466c5a15db5f3
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-12-25 15:58:50 +0530

    Update wps-pwn.sh

--- wps-pwn.sh
+++ wps-pwn.sh
@@ -85,7 +85,7 @@ find ~/conf_bak -size 0 -print -delete
 
 sudo mkdir -p /etc/wpa_supplicant/backup
 sudo cp /etc/wpa_supplicant/wpa_supplicant-wlan1.conf /etc/wpa_supplicant/backup/wpa_supplicant-wlan1.conf.backup_`date +"%H:%M:%S:%d-%b-%Y"` 
-find /etc/wpa_supplicant/ -size 0 -print -delete
+sudo find /etc/wpa_supplicant/ -size 0 -print -delete
 sudo bash -c 'cat tmpfile5 >> /etc/wpa_supplicant/wpa_supplicant-wlan1.conf'
 sudo awk '!seen[$0]++' /etc/wpa_supplicant/wpa_supplicant-wlan1.conf > /tmp/wpa_supplicant-wlan1.conf 
 sudo mv /tmp/wpa_supplicant-wlan1.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf 
