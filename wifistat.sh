ee96a3b7415eaf536b22af79dd06d8fbd17d610f
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-12-26 23:47:49 +0530

    Update wifistat.sh

--- wifistat.sh
+++ wifistat.sh
@@ -1,31 +1,75 @@
 #!/bin/bash
 
-#Color
- col='\033[0;33m'       # Yellow
- nc='\033[0m'       # No Color
+#Yor
+ Y='\033[0;33m'       # Yellow
+ G='\033[0;32m'       # Greeen
+ nc='\033[0m'       # No Yor
  
-echo -e ${col} "Disable monitor-mode" ${nc}
+echo -e ${G}"Use -r for disable monitor-mode and restart wlan1 interface" ${nc}
+
+# Set the default value for the flag
+flag=0
+
+# Parse the command line arguments
+while getopts ":r" opt; do
+  case $opt in
+    r)
+      flag=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Check the value of the flag
+if [ $flag -eq 1 ]; then
+  # Do something if the flag is set
+echo -e ${Y} "Disable monitor-mode" ${nc}
 sudo ip link set wlan1 down
 sudo iw wlan1 set type managed
 sudo ip link set wlan1 up
-echo "Done"
+echo -e ${G} "Done" ${nc}
 echo "====================================================="
 
-#echo -e ${col} "Scanning wifi" ${nc}
+#echo -e ${Y} "Scanning wifi" ${nc}
 #sudo timeout 30s  wpa_cli -i wlan1 scan
 #sudo timeout 30s  wpa_cli -i wlan1 scan_results > wifiresults
 #cat wifiresults
 
-echo -e ${col} "Restart wpa_supplicant@wlan1.service" ${nc}
+echo -e ${Y} "Restart wpa_supplicant@wlan1.service" ${nc}
 sudo systemctl restart wpa_supplicant@wlan1.service
-echo "Done"
+echo -e ${G} "Done" ${nc}
 echo "====================================================="
 
-echo -e ${col} "Status of wifi connection" ${nc}
+echo -e ${Y} "Status of wifi connection" ${nc}
 sleep 5s
 sudo wpa_cli -i wlan1 status
+echo -e ${G} "Done" ${nc}
 echo "====================================================="
 
-echo -e ${col} "Wifi configuration file" ${nc}
+echo -e ${Y} "Wifi configuration file" ${nc}
 sudo cat /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+echo -e ${G} "Done" ${nc}
 echo "====================================================="
+#  echo "Flag is set"
+
+else
+
+  # Do something else if the flag is not set
+  echo -e ${Y} "Status of wifi connection" ${nc}
+sleep 5s
+sudo wpa_cli -i wlan1 status
+echo -e ${G} "Done" ${nc}
+echo "====================================================="
+  echo -e ${Y} "Ping Status" ${nc}
+ping google.com -O -c 4  
+echo -e ${G} "Done" ${nc}
+#  echo "Flag is not set"
+
+fi
